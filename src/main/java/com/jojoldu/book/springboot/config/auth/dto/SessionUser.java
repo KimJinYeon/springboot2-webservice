edb98a0c5d0736bf5a2675031f1ffb962d5e8bf8
kimjinyeon <hourglass458@gmail.com>
2022-04-21 21:48:11 +0900

    20220421 updates

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -2,10 +2,13 @@ package com.jojoldu.book.springboot.config.auth.dto;
 
 import com.jojoldu.book.springboot.domain.user.User;
 import lombok.Getter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 import java.io.Serializable;
 
 @Getter
+@Scope(value = "session",proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class SessionUser implements Serializable {
     private String name;
     private String email;
