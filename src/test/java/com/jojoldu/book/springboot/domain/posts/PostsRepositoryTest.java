03ed9b5c31b088fa16def1c885e93cdde1382027
kimjinyeon <hourglass458@gmail.com>
2022-05-17 01:55:17 +0900

    20220517 updates

--- src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -2,12 +2,12 @@ package com.jojoldu.book.springboot.domain.posts;
 
 
 import org.apache.tomcat.jni.Local;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,14 +15,14 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PostsRepositoryTest {
 
     @Autowired // 스프링이 관리하는 빈(bean)을 주입받습니다.
     PostsRepository postsRepository;
 
-    @After // Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정
+    @AfterEach // Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정
     public void cleanup() {
         postsRepository.deleteAll();
     }
